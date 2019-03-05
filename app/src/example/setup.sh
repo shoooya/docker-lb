fbbefdf25eea4399d9863bcde026d17d943c6fb5
shoooya <shoooya17@gmail.com>
2019-03-06 00:14:04 +0900

    slave設定しようとした形跡をとりあえず

--- app/src/example/setup.sh
+++ app/src/example/setup.sh
@@ -1,8 +1,17 @@
 #!/bin/ash
+echo 'change directory'
 cd /var/www/example
+pwd
+echo 'create .env'
 cp .env.example .env
-php artisan key:generate
+echo 'run composer'
 /bin/composer install
+echo 'key generate'
+php artisan key:generate
+echo 'run migration'
 php artisan migrate
+echo 'run seeding'
 php artisan db:seed --class=ExampleTableSeeder
+echo 'change permission'
 chmod -R 777 storage/
+echo 'done'
